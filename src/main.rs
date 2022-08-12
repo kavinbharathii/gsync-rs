f24ad1cd4b91f6ce58cc5743f16646fd9991e1dd
cipherdot <r.m.kavinbharathi@gmail.com>
2022-08-12 19:05:49 +0530

    comments

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
 // gsync app
+
+// required crates
 use std::process::Command; 
 use std::io::Write;
 use std::io;
 use colored::*;
 
 fn main() {
+    // user prompt
     print!("commit message >>> ");
     io::stdout().flush().unwrap(); 
 
+    // user input for commit message
     let mut message = String::new();
     io::stdin().read_line(&mut message).expect("Failed to read message"); 
+    
+    // git initialize, add files and commit
     let _ = Command::new("git").arg("init").output();
     let _ = Command::new("git").args(["add", "."]).output();
+
+    // capture the status of the commit action to determine if 
+    // a change was commited or not
     let status = Command::new("git").args(["commit", "-m", &message]).status().expect("failed to commit");
 
     match status.code() {
         Some(code) => match code {
+            // if the status code is 1, then there is no changes to commit
             1 => {
                 println!("no changes to commit, {}", "process terminated".red());
             },
+
+            // else commit the changes with the given message
             _ => {
                 println!("{}, ::commit: {}", "Committed successfully".green(), message.blue());
             }
